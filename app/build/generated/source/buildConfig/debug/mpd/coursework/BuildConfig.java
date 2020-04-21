ba7cdec07622c8b8fdd6d4ab77e658e2c15f6fa9
Josenatr <adam.hosie98@gmail.com>
2020-04-21 01:11:55 +0100

    Added APK

---
+++ app/build/generated/source/buildConfig/debug/mpd/coursework/BuildConfig.java
@@ -0,0 +1,17 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package mpd.coursework;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "mpd.coursework";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from build type: debug
+  public static final String CURRENT_INCIDENTS = "https://trafficscotland.org/rss/feeds/currentincidents.aspx";
+  public static final String CURRENT_ROADWORKS = "https://trafficscotland.org/rss/feeds/roadworks.aspx";
+  public static final String PLANNED_ROADWORKS = "https://trafficscotland.org/rss/feeds/plannedroadworks.aspx";
+}
